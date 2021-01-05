383fc52ceb012c5e012aed5140e1ab34dd83dfc6
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-05 20:04:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      if @@item.empty?
-        resp.write "Item not found"
-      else
-        @@item.each do |items|
-          resp.write "#{items}\n"
-      end
+    item_name = req.path.split("/items/").last
+    if item =@@items.find{|i| i.name == item_name}
+      resp.write item.price
+    else 
+      resp.status = 400
+      resp.write "Item not found"
+    end
     else
       resp.write "3.42 Route not found"
       resp.status = 404
