41ef3db11821fb620251884a9d9494a5b9184151
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-05 19:49:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/songs"
-      resp.write "You requested the songs"
+    if req.path=="/items"
+      resp.write "You requested the items"
     else
       resp.write "Route not found"
       resp.status = 404
